65f54052b954bebb01b043118a02c67d9d4f73e7
mostafaznv <mostafa.zeinivand@gmail.com>
2023-03-05 03:18:04 +0330

    add bytes value to settings file

--- config/settings.php
+++ config/settings.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    'bytes-in-mb' => 1048576,
+
     'mail-to'     => explode(',', env('EMAIL_SEND_TO', [])),
     'telegram-to' => explode(',', env('TELEGRAM_BOT_TOKEN', [])),
 ];
