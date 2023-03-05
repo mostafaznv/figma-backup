f269d99b2ae91d0add7993ede7195ac34827fa3b
mostafaznv <mostafa.zeinivand@gmail.com>
2023-03-05 13:05:30 +0330

    bugfix

--- resources/views/project/view.blade.php
+++ resources/views/project/view.blade.php
@@ -55,8 +55,7 @@
                                         class="bg-white dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100"
                                         @class(['border-b' => !$loop->last])
                                     >
-                                        <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $history->year }}</th>
-                                        <td class="py-4 px-6">{{ $backup->name }}</td>
+                                        <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $backup->name }}</th>
                                         <td class="py-4 px-6">{{ byteToMb($backup->size) }}</td>
                                         <td class="py-4 px-6">
                                             <a href="{{ $backup->link }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" download>{{ __('Download') }}</a>
