9f66a287d9dba3c67df9f8750937eb1038a9fadc
mostafaznv <mostafa.zeinivand@gmail.com>
2023-03-05 03:52:41 +0330

    bugfix

--- config/settings.php
+++ config/settings.php
@@ -4,5 +4,5 @@ return [
     'bytes-in-mb' => 1048576,
 
     'mail-to'     => explode(',', env('EMAIL_SEND_TO', [])),
-    'telegram-to' => explode(',', env('TELEGRAM_BOT_TOKEN', [])),
+    'telegram-to' => explode(',', env('TELEGRAM_SEND_TO', [])),
 ];
