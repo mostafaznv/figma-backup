210c3f39b215bd8fa990f4b91f927393d6cd6c06
mostafaznv <mostafa.zeinivand@gmail.com>
2023-03-07 01:17:53 +0330

    configurable x-sendfile base-path

--- config/x-sendfile.php
+++ config/x-sendfile.php
@@ -3,7 +3,7 @@
 return [
     'server' => 'Nginx',
 
-    'base-path' => $_SERVER['DOCUMENT_ROOT'] . '/../',
+    'base-path' => env('X_SENDFILE_BASE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/../'),
 
     'cache'                 => true,
     'cache-control-max-age' => 2592000
