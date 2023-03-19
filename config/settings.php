91c3428607f3c590b991a316260a9a9677569706
mostafaznv <mostafa.zeinivand@gmail.com>
2023-03-19 14:54:23 +0330

    wip

--- config/settings.php
+++ config/settings.php
@@ -7,7 +7,7 @@ return [
     'file-expiry-days'       => 10,
     'telegram-max-file-size' => env('TELEGRAM_MAX_FILE_SIZE', 49), // MB
 
-    'mail-to'     => explode(',', env('EMAIL_SEND_TO', [])),
+    'mail-to'     => explode(',', env('SEND_WARNING_EMAIL_TO', [])),
     'telegram-to' => [
         'backups'  => explode(',', env('TELEGRAM_BACKUPS_SEND_TO', [])),
         'warnings' => explode(',', env('TELEGRAM_WARNINGS_SEND_TO', [])),
